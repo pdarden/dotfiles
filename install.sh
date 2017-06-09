a22ce5e9e23e6375e7636094fcfaaf216f49bb9f
Parinda Darden <parinda.darden@gmail.com>
2017-06-09 12:19:02 -0400

    Install python for deoplete vim plugin

--- install.sh
+++ install.sh
@@ -27,9 +27,12 @@ brew install battery
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
 echo 'source ~/.asdf/asdf.fish' >> ~/.config/fish/config.fish
 mkdir -p ~/.config/fish/completions; and cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+asdf plugin-add python https://github.com/tuvistavie/asdf-python.git
+asdf install python 3.6.1
+asdf global python 3.6.1
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \                                                                                      Mon Jun  5 23:57:47 2017
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.config/nvim/plugged
 omf install bobthefish direnv fzf spark wifi-password
-nvim -u ~/.config/nvim/init.vim +PluginInstall +qall
 omf reload
+nvim -u ~/.config/nvim/init.vim +PluginInstall +qall
